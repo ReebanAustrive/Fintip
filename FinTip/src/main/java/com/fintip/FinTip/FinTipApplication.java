8c91ccddcbed1258336473b3aaa8520bb857d65b
Madreeban0 <madreeban277@gmail.com>
2026-02-04 11:46:02 +0530

    Done with Setting up the environment

---
+++ FinTip/src/main/java/com/fintip/FinTip/FinTipApplication.java
@@ -0,0 +1,13 @@
+package com.fintip.FinTip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTipApplication.class, args);
+	}
+
+}
---
+++ FinTip/src/test/java/com/fintip/FinTip/FinTipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintip.FinTip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
